ad8f7a6b3eb2028a4f3246ceaecd45111397ceb0
Taegon Kim <gonom9@gmail.com>
2017-01-07 23:02:23 +0900

    Initial Commit

---
+++ Highlight/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Highlight
+//
+//  Created by Taegon Kim on 07/01/2017.
+//  Copyright © 2017 Taegon Kim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
